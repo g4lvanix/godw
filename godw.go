d0002221b6ef2cf6e79b6493ae75b9e452bf572f
g4lvanix <elia@dl1ye.com>
2018-08-25 10:40:14 +0200

    now closing file properly, simplified output and variable naming

--- godw.go
+++ godw.go
@@ -12,26 +12,25 @@ import (
 
 func getWords(fname string) ([]string, error) {
 	var words []string
-	file, err := os.Open(fname)
 
+	file, err := os.Open(fname)
 	if err != nil {
 		return words, err
 	}
+	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		l := strings.Fields(scanner.Text())
 		words = append(words, l[1])
 	}
-
 	return words, nil
 }
 
 func chooseWords(words []string, n int) []string {
 	var out []string
 	for i := 0; i < n; i++ {
-		k, _ := rand.Int(rand.Reader,
-			big.NewInt(int64(len(words))))
+		k, _ := rand.Int(rand.Reader, big.NewInt(int64(len(words))))
 		out = append(out, words[k.Int64()])
 	}
 	return out
@@ -44,17 +43,17 @@ func main() {
 		fmt.Print("\nDiceware password generator\n\n" +
 			"Usage: ./godw FILE LENGTH\n\n" +
 			"FILE   The diceware word file\n" +
-			"LENGTH Number of words in the passphrase\n")
+			"LENGTH Number of words in the passphrase\n\n")
 		os.Exit(0)
 	}
 
 	filename := args[1]
-	nwords, err := strconv.Atoi(args[2])
+	length, err := strconv.Atoi(args[2])
 
 	if err != nil {
 		panic(err)
-	} else if nwords < 1 {
-		fmt.Print("Password must contain at least 1 word")
+	} else if length < 1 {
+		fmt.Println("Passphrase must contain at least 1 word")
 		os.Exit(1)
 	}
 
@@ -64,7 +63,7 @@ func main() {
 		panic(err)
 	}
 
-	passphrase := strings.Join(chooseWords(words, nwords), " ")
+	passphrase := strings.Join(chooseWords(words, length), " ")
 
-	fmt.Print(passphrase + "\n")
+	fmt.Println(passphrase)
 }
